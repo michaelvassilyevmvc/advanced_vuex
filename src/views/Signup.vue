db646a1d8603bb7a0c4b353e804b9027d9e03182
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-07 16:40:51 +0600

    Create signup in store

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,28 +3,31 @@
     <h3>Sign up</h3>
 
     <label for="email">Email:</label>
-    <input type="email" name="email" v-model="email" required>
+    <input type="email" name="email" v-model="email" required />
 
     <label for="email">Password:</label>
-    <input type="password" name="password" v-model="password" required>
+    <input type="password" name="password" v-model="password" required />
 
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
   setup() {
-    const email = ref('')
-    const password = ref('')
+    const email = ref("");
+    const password = ref("");
+
+    const store = useStore();
 
     const handleSubmit = () => {
-      console.log(email.value, password.value)
-    }
+      store.dispatch("signup", { email, password });
+    };
 
-    return { handleSubmit, email, password }
-  }
-}
+    return { handleSubmit, email, password };
+  },
+};
 </script>
